890a6c3c4cb81fa3a8c0e7a7be06b7fd9a2fa6bd
jOBISCREIDO <72766169+jOBISCREIDO@users.noreply.github.com>
2022-08-03 14:46:20 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,20 @@
+var eNome = document.querySelector('input')
+var button = document.querySelector('button')
+
+function preload() {
+    
+
+
+}
+
+function draw() {
+    
+
+
+}
+
+button.onclick = function() {
+    var nome = prompt('Qual é o seu nome?');
+    alert('Olá ' + nome + ', é um prazer te ver!');
+  }
+
